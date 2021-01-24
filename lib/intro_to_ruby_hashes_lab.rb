4b256470ee99bce3cc6bf671e792e0e7d32c055c
Val_1 S_z <csa.aiden@gmail.com>
2021-01-24 05:30:10 +0000

    Done.

---
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,105 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+introduction("Steven")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Maria", "Ruby")
+
+
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("Elena")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def introduction(name)
+#  puts "Hi, my name is #{name}."
+#end
+
+#def introduction_with_language(name, language)
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
+
+#def introduction_with_language_optional(name, language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
